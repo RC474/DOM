d780355d0ad3a6787023b01f6c80471215d440b9
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-14 18:20:08 -0500

    DOM selectors

---
+++ selector.js
@@ -0,0 +1,6 @@
+var p = document.getElementById("first");
+var p1 = document.getElementsByClassName("special");
+var p = document.querySelectorAll("#first")
+var p = document.getElementsByTagName("p")[0]
+
+p.style.color = "red";
